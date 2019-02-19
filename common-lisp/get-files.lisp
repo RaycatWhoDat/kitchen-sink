603a58105c1c24aeffc31c3cb7c3aa3eb881857d
Ray Perry <rmperry09@gmail.com>
2019-02-19 06:21:07 -0600

    Common Lisp | Still trying to reimplement `get-files`.

--- common-lisp/get-files.lisp
+++ common-lisp/get-files.lisp
@@ -1,18 +1,17 @@
-(defvar *ignored-paths* '("./" "../" ".git/" "node_modules/"))
-
-(labels ((print-file (directory-pathname)
-           (unless (some (lambda (ignored-path)
-                           ;; (print ignored-path)
-                           ;; (print (car (last (pathname-directory directory-pathname))))
-                           (unless (eq (car (last (pathname-directory directory-pathname))) :UP)
-                             (or (search ignored-path (car (last (pathname-directory directory-pathname))))
-                                 (eql ignored-path (car (last (pathname-directory directory-pathname))))))) *ignored-paths*)
-             (format t "~A~%" directory-pathname)
-             (let ((files (uiop:directory-files directory-pathname)))
-               (loop :for file :in files
-                     :do (format t "~A~A~:[~;.~A~]~%"
-                                 directory-pathname
-                                 (pathname-name file)
-                                 (pathname-type file)
-                                 (pathname-type file)))))))
-  (uiop:collect-sub*directories "../" t t #'print-file))
+(let ((ignored-paths '("./" "../" ".git/" "node_modules/")))
+  (labels ((print-file (directory-pathname)
+             (if (map nil (lambda (ignored-path)
+                             (let ((current-directory-name (car (last (pathname-directory directory-pathname)))))
+                               (print ignored-path)
+                               (print current-directory-name)
+                               (unless (eq current-directory-name :UP)
+                                 (or (search ignored-path current-directory-name))
+                                 (eql ignored-path current-directory-name)))) ignored-paths) (return))
+               (format t "~A~%" directory-pathname)
+               (let ((files (uiop:directory-files directory-pathname)))
+                 (dolist (file files)
+                   (format t "~A~A~:[~;~:*.~A~]~%"
+                           directory-pathname
+                           (pathname-name file)
+                           (pathname-type file))))))
+    (uiop:collect-sub*directories "../" t t #'print-file)))
