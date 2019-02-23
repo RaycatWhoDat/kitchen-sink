ba8b017230b1a99da85195ef4976b7f66cf831c0
Ray Perry <rperry@runoranj.com>
2019-02-22 22:05:41 -0600

    Haxe | Update `ScryfallSearch`.

--- haxe/ScryfallSearch.hx
+++ haxe/ScryfallSearch.hx
@@ -2,14 +2,41 @@ using haxe.Http;
 
 class ScryfallSearch {
   static function main() {
-    var query: String = StringTools.urlEncode("snea att").split("%20").join("+");
-    var results: Array<Dynamic> = haxe.Json.parse(Http.requestUrl("https://api.scryfall.com/cards/search?q=" + query)).data;
+    var query: String = StringTools
+                        .urlEncode("sne attack")//Sys.args().join(" "))
+                        .split("%20")
+                        .join("+");
 
-    for (card in results) {
-      Sys.println(card.name + ' ' + card.mana_cost);
-      Sys.println(card.type_line);
-      Sys.println(card.oracle_text);
+    if (query.length <= 0) {
+      trace("No cards found.");
+      return;
     }
+    
+    // var results: Array<Dynamic> = haxe.Json.parse(Http.requestUrl("https://api.scryfall.com/cards/search?q=" + query)).data;
 
+    var results: Array<Dynamic> = haxe.Json.parse(Http.requestUrl("https://jsonplaceholder.typicode.com/posts"));
+
+    if (results.length <= 0) {
+      // trace("No cards found.");
+      return;
+    }
+    
+    // for (card in results) {
+    //   Sys.println(card.name + " " + card.mana_cost);
+    //   Sys.println(card.type_line);
+    //   Sys.println(card.oracle_text);
+      
+    //   if (card.power != null || card.toughness != null) {
+    //     Sys.println(card.power + "/" + card.toughness);
+    //   }
+    // }
+
+    for (post in results) {
+      Sys.println(post.id + " | " + post.userId);
+      Sys.println("");
+      Sys.println(post.title);
+      Sys.println(post.body);
+      Sys.println("");
+    }
   }
 }
