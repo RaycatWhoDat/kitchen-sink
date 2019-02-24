75260b87408932155b4e275034ec2714c688efb3
Ray Perry <rmperry09@gmail.com>
2019-02-23 20:20:57 -0600

    D | Per #d feedback.

--- d/get-files.d
+++ d/get-files.d
@@ -8,34 +8,33 @@ import std.string: indexOf;
 import std.stdio: writeln;
 import std.getopt;
 
-int indentationLevel = 2;
-int traversalLevel = -1;
+enum IndentationLevel {
+  TWO_SPACES = 2,
+  FOUR_SPACES = 4
+}
+
 string[] ignoredPaths = ["/.git", "/node_modules", "/target"];
 
-string generateIndent(int traversalLevel = 0) {
+string generateIndent(int traversalLevel = 0) pure {
   string indentation = "";
-  foreach (index; 0..(indentationLevel * traversalLevel)) {
-    indentation ~= " ";
-  }
+  foreach (index; 0 .. IndentationLevel.TWO_SPACES * traversalLevel) indentation ~= " ";
+
   return indentation;
 }
 
-void printFiles(string directoryPath = getcwd()) {
-  traversalLevel += 1;
+void printFiles(string directoryPath, int traversalLevel = 0) {
+iterateOverFiles:
   foreach (entry; dirEntries(directoryPath, SpanMode.shallow)) {
-    bool isPathIgnorable = false;
     foreach (pathToIgnore; ignoredPaths) {
-      if (indexOf(entry.name, pathToIgnore) >= 0) isPathIgnorable = true;
+      if (indexOf(entry.name, pathToIgnore) >= 0) break iterateOverFiles;
     }
-    if (isPathIgnorable) break;
     writeln(generateIndent(traversalLevel), entry.name.replace(directoryPath, ""), entry.isDir ? "/" : "");
-    if (entry.isDir) printFiles(entry.name ~ "/");
+    if (entry.isDir) printFiles(entry.name ~ "/", traversalLevel + 1);
   }
-  traversalLevel -= 1;
 }
 
 void main(string[] args) {
-  string directoryPath;
+  string directoryPath = getcwd();
   getopt(args, "directory", &directoryPath);
   printFiles(directoryPath);
 }
