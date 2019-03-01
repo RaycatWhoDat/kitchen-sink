825ecaf2fa2b9a8b8d898f0d3d13dbe88cad7e70
Ray Perry <rmperry09@gmail.com>
2019-03-01 03:30:14 -0600

    Haxe | LOVE things!

--- haxe/LuaTestApp.hx
+++ haxe/LuaTestApp.hx
@@ -1,5 +1,37 @@
-class LuaTestApp {
-  static function main() {
-    trace("This is a test.");
+import love.Love;
+import love.graphics.GraphicsModule as Lg;
+
+class Game {
+  private var testMessage: String = "This is a test.";
+
+  public function new() {
+    
+  }
+  
+  public function load(args: lua.Table<Dynamic, Dynamic>) {
+    
+  }
+
+  public function update(dt: Float) {
+
+  }
+
+  public function draw() {
+    Lg.print(this.testMessage, 10, 10);
   }
 }
+
+
+class LuaTestApp {
+  public static function main() {
+    var game = new Game();
+    
+    Love.load = game.load.bind();
+    Love.update = game.update.bind();
+    Love.draw = game.draw.bind();
+  }
+}
+
+// Local Variables:
+// compile-command: "haxe luaTestApp.hxml"
+// End:
