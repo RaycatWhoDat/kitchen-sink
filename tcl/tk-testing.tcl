604f1d7bfe849d9243a489e62e9b04b288a93366
Ray Perry <rperry@runoranj.com>
2019-03-06 08:02:39 -0600

    Tcl | Beginnings of the habit tracker.

--- tcl/tk-testing.tcl
+++ tcl/tk-testing.tcl
@@ -1,34 +1,15 @@
 package require Tk
 
-set about "minEd - a minimal editor
+wm title . "Habit Tracker"
+grid [ttk::frame .base -padding "3 3 12 12"] -column 0 -row 0 -sticky nwes
+grid columnconfigure . 0 -weight 1; grid rowconfigure . 0 -weight 1
 
-F1: help
-F2: load
-F3: save
-"
+grid [label .base.hello1 -text "Hello!"] -column 1 -row 2 -sticky w
+grid [label .base.hello2 -text "Hello!"] -column 2 -row 2 -sticky w
+grid [label .base.hello3 -text "Hello!"] -column 3 -row 2 -sticky w
 
-pack [scrollbar .y -command ".t yview"] -side right -fill y
-pack [text .t -wrap word -yscrollc ".y set" -undo 1] -side right -fill both -expand 1
+foreach w [winfo children .base] {grid configure $w -padx 5 -pady 5}
 
-bind . <F1> {tk_messageBox -message $about}
-bind . <F2> {loadText .t [tk_getOpenFile]}
-bind . <F3> {saveText .t [tk_getSaveFile]}
-
-proc loadText {w fn} {
-   if {$fn==""} return
-   wm title . [file tail $fn]
-   set fp [open $fn]
-   $w delete 1.0 end
-   $w insert end [read $fp]
-   close $fp
-}
-
-proc saveText {w fn} {
-   if {$fn==""} return
-   set fp [open $fn w]
-   puts -nonewline $fp [$w get 1.0 "end - 1 c"]
-   close $fp
-}
-
-if {$argv != ""} {loadText .t [lindex $argv 0]}
-
+# Local Variables:
+# compile-command: "tclsh ./tk-testing.tcl"
+# End: 
