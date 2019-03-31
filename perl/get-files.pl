6e3438eedf7b8a95a51093ec0d410ee6e365e537
Ray Perry <rmperry09@gmail.com>
2019-03-30 22:20:01 -0500

    Perl | List files.

--- perl/get-files.pl
+++ perl/get-files.pl
@@ -3,21 +3,28 @@
 use strict;
 use warnings;
 
-use File::Find;
+use File::Find "find";
 
 my $max_indent_level = 2;
-my %ignored_paths = map { $_ => 1 } ('.', '..', '.git', 'node_modules', 'target', 'love', 'test_app', 'zef');
+my %ignored_paths = map { $_ => 1 } qw(. .. .git node_modules target love test_app zef);
+my $directory = '..';
+$directory = shift @ARGV if $#ARGV + 1 > 0;
 
 sub list_files {
-    return if exists($ignored_paths{$_});
-    print("$File::Find::name\n");
+    my $depth = $File::Find::dir =~ tr[/][];
+    if (exists($ignored_paths{$_})) {
+        $File::Find::prune = 1 if $File::Find::name ne $directory;
+        return;
+    }
+    print(' ' x ($max_indent_level * $depth) . "$_\n");
 }
 
-find(\&list_files, ('..'))
+find(\&list_files, $directory);
 
 
 # Local Variables:
-# compile-command: "perl ./get-files.pl"
+# compile-command: "perl ./get-files.pl .."
 # End:
 
+    
     
