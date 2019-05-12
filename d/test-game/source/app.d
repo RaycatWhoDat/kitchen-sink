1e922dc31ea811019be27e74e256dd2d3f7f8a6f
Ray Perry <rperry@runoranj.com>
2019-05-12 08:45:39 -0500

    D | Still trying to get this working.

--- d/test-game/source/app.d
+++ d/test-game/source/app.d
@@ -14,22 +14,26 @@ void init() {
   loadSDLMixer();
   loadSDLTTF();
 
-  auto window = SDL_CreateWindow(toStringz("This is a test."),SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500, 500, SDL_WINDOW_SHOWN);
+  auto window = SDL_CreateWindow(
+      toStringz("This is a test."),
+      SDL_WINDOWPOS_UNDEFINED,
+      SDL_WINDOWPOS_UNDEFINED,
+      500,
+      500,
+      SDL_WINDOW_OPENGL);
 
   if (window == null) {
     writeln("No window found.");
     return;
   }
 
-  auto screenSurface = SDL_GetWindowSurface(window);
-  // auto cover = IMG_Load("image.jpg");
+  auto renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  auto cover = SDL_CreateTextureFromSurface(renderer, SDL_LoadBMP("./image.bmp"));
 
-  // if (cover == null) {
-  //   writeln("No image.");
-  //   return;
-  // }
-  
-  SDL_UpdateWindowSurface(window);
+  SDL_RenderClear(renderer);
+  SDL_RenderCopy(renderer, cover, null, null);
+  SDL_RenderPresent(renderer);
+    
   SDL_Delay(2000);
   SDL_DestroyWindow(window);
   SDL_Quit();
