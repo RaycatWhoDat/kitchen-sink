d251259bd076f11264995a64a1f7e5ab64370fbf
Ray Perry <rmperry09@gmail.com>
2019-05-19 17:03:29 -0500

    Racket | Indentation.

--- racket/get-files.rkt
+++ racket/get-files.rkt
@@ -7,9 +7,9 @@
       (let ((current-path (path->string entry)))
         (unless (for/or ((ignored-path '(".git" "love" "target" "dist" ".dub" "node_modules")))
                   (string-contains? current-path ignored-path))
-        (printf (string-append (make-string (* 2 traversal-level) #\space) "~a~%") current-path)
-        (when (directory-exists? (path->directory-path (build-path directory-path current-path)))
-          (get-files (string-append directory-path "/" current-path) (+ traversal-level 1)))))))
+          (printf (string-append (make-string (* 2 traversal-level) #\space) "~a~%") current-path)
+          (when (directory-exists? (path->directory-path (build-path directory-path current-path)))
+            (get-files (string-append directory-path "/" current-path) (+ traversal-level 1)))))))
 
 (get-files)
 
