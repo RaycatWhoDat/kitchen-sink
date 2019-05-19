453ad41b872e1216c282515dbfa7f4a95ce5b7ba
Ray Perry <rmperry09@gmail.com>
2019-05-19 16:40:04 -0500

    Racket | `get-files`.

--- racket/get-files.rkt
+++ racket/get-files.rkt
@@ -2,12 +2,14 @@
 
 (require racket/path racket/string)
 
-(define (get-files directory-path)
-  (parameterize ([current-directory directory-path])
-    (for ([entry (in-directory)])
-      (unless (for/or ([ignored-path '(".git/" "love/" "target/" "dist/" ".dub/" "node_modules")])
-                (string-contains? (path->string entry) ignored-path))
-        (printf "~a\n" (file-name-from-path entry))))))
+(define (get-files (directory-path "..") (traversal-level 0))
+    (for ((entry (directory-list directory-path)))
+      (let ((current-path (path->string entry)))
+        (unless (for/or ((ignored-path '(".git" "love" "target" "dist" ".dub" "node_modules")))
+                  (string-contains? current-path ignored-path))
+        (printf (string-append (make-string (* 2 traversal-level) #\space) "~a~%") current-path)
+        (when (directory-exists? (path->directory-path (build-path directory-path current-path)))
+          (get-files (string-append directory-path "/" current-path) (+ traversal-level 1)))))))
 
-(get-files "..")
+(get-files)
 
