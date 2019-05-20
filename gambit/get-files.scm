afddfd42ecd0dea5224726c953e0668cce1743a9
Ray Perry <rperry@runoranj.com>
2019-05-20 17:21:34 -0500

    Racket, Gambit | `get-files`.

---
+++ gambit/get-files.scm
@@ -0,0 +1,12 @@
+#!/usr/bin/env gsi-script
+
+(define (get-files #!optional (current-directory "..") (traversal-level 0))
+  (for-each (lambda (entry) (print (string-append (make-string (* 2 traversal-level) #\space) entry "\n"))
+      (let ((printed-directory (string-append current-directory "/" entry)))
+        (unless (member entry '(".git" "love" "target" "dist" ".dub" "node_modules"))
+          (if (eq? (file-info-type (file-info printed-directory)) 'directory)
+            (get-files printed-directory (+ traversal-level 1))))))
+      (directory-files current-directory)))
+
+(get-files)
+
--- racket/get-files.rkt
+++ racket/get-files.rkt
@@ -1,3 +1,5 @@
+#!/usr/bin/env racket
+
 #lang racket/base
 
 (require racket/path racket/string)
