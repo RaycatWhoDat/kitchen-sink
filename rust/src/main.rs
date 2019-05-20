10c03b80cd5e29a56fc7576b7bfea801dceb4bba
Ray Perry <rmperry09@gmail.com>
2019-05-19 23:33:31 -0500

    Rust | `get-files`, Phase 2.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,15 +3,27 @@ use std::fs::read_dir;
 use std::path::Path;
 
 fn print_files(directory: &Path) -> Result<()> {
+    // let ignored_paths = [
+    //     ".git",
+    //     "love",
+    //     "target",
+    //     "dist",
+    //     ".dub",
+    //     "node_modules"
+    // ];
+    
     for entry in read_dir(directory)? {
-        let entry = entry?;
+        let entry = &entry?;
         println!("{:?}", entry.path());
-        // let entryPath = Path::new(&entry);
-        // if entryPath.is_dir() { print_files(entryPath); }
+
+        if Path::new(&entry.path()).is_dir() {
+            // Path::new(&entry.path()).to_str().unwrap().contains("target") {
+            assert!(print_files(Path::new(&entry.path())).is_ok());
+        }
     }
     Ok(())
 }
 
 fn main() {
-    assert!(print_files(Path::new("../")).is_ok());
+    assert!(print_files(Path::new("../..")).is_ok());
 }
