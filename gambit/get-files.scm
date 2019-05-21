6847e23e390b12bda37c5208ac5fe148a4b85cae
Ray Perry <rperry@runoranj.com>
2019-05-20 21:02:02 -0500

    Racket, Gambit | Small updates.

--- gambit/get-files.scm
+++ gambit/get-files.scm
@@ -1,6 +1,7 @@
 #!/usr/bin/env gsi-script
 
 (define (get-files #!optional (current-directory "..") (traversal-level 0))
+  "This recursively prints the files in CURRENT-DIRECTORY."
   (for-each (lambda (entry) (print (string-append (make-string (* 2 traversal-level) #\space) entry "\n"))
       (let ((printed-directory (string-append current-directory "/" entry)))
         (unless (member entry '(".git" "love" "target" "dist" ".dub" "node_modules"))
--- racket/get-files.rkt
+++ racket/get-files.rkt
@@ -7,10 +7,14 @@
 (define (get-files (directory-path "..") (traversal-level 0))
   (for ((entry (directory-list directory-path)))
     (let ((current-path (path->string entry)))
+      (printf (string-append (make-string (* 2 traversal-level) #\space) "~a~%") current-path)
       (unless (for/or ((ignored-path '(".git" "love" "target" "dist" ".dub" "node_modules")))
                 (string-contains? current-path ignored-path))
-        (printf (string-append (make-string (* 2 traversal-level) #\space) "~a~%") current-path)
         (when (directory-exists? (path->directory-path (build-path directory-path current-path)))
           (get-files (string-append directory-path "/" current-path) (+ traversal-level 1)))))))
 
 (get-files)
+
+;; Local Variables:
+;; compile-command: "racket ./get-files.rkt"
+;; End:
