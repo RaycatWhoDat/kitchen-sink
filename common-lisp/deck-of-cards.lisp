3b607e2ef26037dfe0eb970652a54fa8502e7370
Ray Perry <rperry@runoranj.com>
2019-06-18 14:51:49 -0500

    Common Lisp | Deck of cards update.

--- common-lisp/deck-of-cards.lisp
+++ common-lisp/deck-of-cards.lisp
@@ -1,6 +1,27 @@
-(defun print-deck ()
-  (declare (optimize (speed 3) (safety 0) (debug 0) (compilation-speed 3)))
+(defun make-deck ()
+  "Returns a fresh DECK of cards."
   (loop for index to 51
     for value = (+ (mod index 13) 1)
     for suit = (nth (mod value 4) '(♥ ♦ ♠ ♣))
     collect (cons value suit)))
+
+(defun shuffle (deck)
+  "Returns DECK in a randomized order."
+  (sort deck #'(lambda (card1 card2)
+                 (declare (ignore card1 card2))
+                 (zerop (random 2)))))
+
+(defun look-at-top-card (deck)
+  "Returns the value of top card of the DECK."
+  (car deck))
+
+(defun draw-card (deck)
+  "Returns the top card of the DECK."
+  (values (look-at-top-card deck) (cdr deck)))
+
+(let ((deck (shuffle (make-deck))))
+  (loop repeat 5
+    do (multiple-value-bind (drawn-card rest-of-cards) (draw-card deck)
+         (declare (ignore drawn-card))
+         (setf deck rest-of-cards)))
+  (length deck))
