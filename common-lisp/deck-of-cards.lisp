8bf5dd6cbacbbd7b56fd3a9f5998e347cc181993
Ray Perry <rperry@runoranj.com>
2019-06-20 14:55:54 -0500

    Common Lisp | It's slowly becoming an obsession.

--- common-lisp/deck-of-cards.lisp
+++ common-lisp/deck-of-cards.lisp
@@ -6,15 +6,15 @@
      :accessor value)
     (suit
       :initarg :suit
-      :accessor suit)))
+      :accessor suit)
+    (color
+     :initarg :color
+     :accessor color)))
 
-(defun make-card (value suit)
-  (make-instance 'card :value value :suit suit))
-
-(defgeneric pretty-print-value (card-face)
+(defgeneric pretty-print-value (card)
   (:documentation "Returns the display value of the given CARD.")
-  (:method ((card-face card))
-    (let ((raw-value (value card-face)))
+  (:method (card)
+    (let ((raw-value (value card)))
       (cond ((= raw-value 1) 'A)
         ((= raw-value 11) 'J)
         ((= raw-value 12) 'Q)
@@ -23,32 +23,37 @@
 
 ;; DECK
 
-(defclass deck ()
+(defclass deck-of-cards ()
   ((cards
      :initarg :cards
      :accessor cards)))
 
-(defun make-deck-of-cards (list-of-cards)
-  (make-instance 'deck :cards list-of-cards))
+(defun make-deck-of-cards ()
+  (make-instance 'deck-of-cards
+    :cards (loop for index to 51
+             for value = (+ (mod index 13) 1)
+             for suit = (nth (mod value 4) '(♥ ♠ ♦ ♣))
+             for color = (nth (mod value 2) '(red black))
+             collect (make-instance 'card :value value :suit suit :color color))))
 
 (defgeneric shuffle (deck-of-cards)
   (:documentation "Returns DECK with CARDS in a randomized order.")
-  (:method ((deck-of-cards deck))
+  (:method (deck-of-cards)
     (setf (cards deck-of-cards)
       (sort (cards deck-of-cards)
-        #'(lambda (card1 card2)
+        (lambda (card1 card2)
             (declare (ignore card1 card2))
             (zerop (random 2)))))
     deck-of-cards))
 
 (defgeneric look-at-top-card (deck-of-cards)
   (:documentation "Returns the value of top card of the DECK.")
-  (:method ((deck-of-cards deck))
+  (:method (deck-of-cards)
     (car (cards deck-of-cards))))
 
 (defgeneric draw-card (deck-of-cards)
   (:documentation "Returns the top card of the DECK.")
-  (:method ((deck-of-cards deck))
+  (:method (deck-of-cards)
     (let ((drawn-card (look-at-top-card deck-of-cards))
           (rest-of-cards (cdr (cards deck-of-cards))))
       (setf (cards deck-of-cards) rest-of-cards)
@@ -56,24 +61,24 @@
 
 (defgeneric remaining-cards (deck-of-cards)
   (:documentation "Returns the remaining number of CARDS in the DECK.")
-  (:method ((deck-of-cards deck))
+  (:method (deck-of-cards)
     (length (cards deck-of-cards))))
 
 ;;;
 ;;; TEST FUNCTIONS
 ;;;
 
-(defun make-list-of-cards ()
-  "Returns 52 CARD objects."
-  (loop for index to 51
-    for value = (+ (mod index 13) 1)
-    for suit = (nth (mod value 4) '(♥ ♦ ♠ ♣))
-    collect (make-card value suit)))
-  
-(let ((deck (shuffle (make-deck (make-list-of-cards)))))
-  (loop repeat 5
-    do (multiple-value-bind (drawn-card rest-of-cards) (draw-card deck)
-         (declare (ignore drawn-card))
-         (setf (cards deck) rest-of-cards)))
-  (remaining-cards deck))
+;; (let ((deck (shuffle (make-deck-of-cards))))
+;;   (loop repeat 5 do (draw-card deck))
+;;   (remaining-cards deck))
+
+;; TODO: Fix the card generation.
+
+(loop for card in (cards (shuffle (make-deck-of-cards)))
+  if (equal (color card) 'black)
+  collect card into black-cards
+  and count t into number-of-black-cards
+  else collect card into red-cards
+  and count t into number-of-red-cards
+  finally (return (values number-of-black-cards black-cards number-of-red-cards red-cards)))
 
