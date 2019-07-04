d89a469267a9abecf628bac502e45fd2f9236c25
Ray Perry <rperry@runoranj.com>
2019-07-04 17:48:55 -0500

    Common Lisp, D | Small updates.

--- common-lisp/exercism-snippets.lisp
+++ common-lisp/exercism-snippets.lisp
@@ -15,12 +15,15 @@
   "Returns T if YEAR is a leap year."
   (zerop (mod year (if (zerop (mod year 100)) 400 4))))
 
+(defun question-p (statement) (char= (char statement (1- (length statement))) #\?))
+(defun shouting-p (statement) (and (notany #'lower-case-p statement) (some #'alpha-char-p statement)))
+(defun silence-p (statement) (zerop (length statement)))
+
 (defun response-for (input)
-  (let ((trimmed-input (string-trim '(#\Space #\Newline #\Backspace #\Tab #\Linefeed #\Page #\Return #\Rubout) input)))
-    (if (zerop (length trimmed-input))
-      "Fine. Be that way!"
-      (let ((question? (char= (char trimmed-input (1- (length trimmed-input))) #\?)))
-        (cond
-          ((notany #'alpha-char-p trimmed-input) (if question? "Sure." "Whatever."))
-          ((notany #'lower-case-p trimmed-input) (if question? "Calm down, I know what I'm doing!" "Whoa, chill out!"))
-          (t (if question? "Sure." "Whatever.")))))))
+  (let ((statement (string-trim '(#\Space #\Newline #\Backspace #\Tab #\Linefeed #\Page #\Return #\Rubout) input)))
+    (cond
+      ((silence-p statement) "Fine. Be that way!")
+      ((and (shouting-p statement) (question-p statement)) "Calm down, I know what I'm doing!")
+      ((shouting-p statement) "Whoa, chill out!")
+      ((question-p statement) "Sure.")
+      (t "Whatever."))))
