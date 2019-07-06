0a9b94375bd0e445b68869b06d738fe39100be32
Ray Perry <rmperry09@gmail.com>
2019-07-06 00:19:16 -0500

    Common Lisp | Bad header.

--- common-lisp/get-files.lisp
+++ common-lisp/get-files.lisp
@@ -1,5 +1,3 @@
-#!/usr/bin/env sbcl --script
-
 (defun print-files (&optional (current-directory ".."))
   "Prints all the files recursively, starting at CURRENT-DIRECTORY."
   (let ((ignored-paths '(".git" "love" "target" "dist" ".dub" "node_modules")))
