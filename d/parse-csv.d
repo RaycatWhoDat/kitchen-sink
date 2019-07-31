0af4e5d7e08eee220e3db18f737ada6816e15025
Ray Perry <rperry@runoranj.com>
2019-07-30 21:22:09 -0500

    D | Simplify.

--- d/parse-csv.d
+++ d/parse-csv.d
@@ -3,23 +3,14 @@
 module parsecsv;
 
 import std.stdio: writefln, File;
-import std.csv: csvReader;
-import std.algorithm: each, joiner;
-import std.typecons: Tuple;
-import std.range.primitives: back;
+import std.algorithm: each;
+import std.range: split, back;
 
 void main(string[] args) {
-
   string filename = args.length > 1 ? args.back : "no_file.csv";
-  auto csv_file = File(filename, "r");
-
-  auto parsed_csv = csv_file
-                    .byLine
-                    .joiner("\n")
-                    .csvReader!(Tuple!(string, string, string, string));
-  
-  parsed_csv.each!(record => writefln("%s, %s, %s, %s", record[0], record[1], record[2], record[3]));
-    
+  File(filename, "r")
+      .byLine()
+      .each!(record => writefln("%(%s, %)", record.split(',')));
 }
 
 // Local Variables:
