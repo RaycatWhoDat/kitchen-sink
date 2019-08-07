24bba4d43d0fe04ff6a1d257440c0721f88382a5
Ray Perry <rperry@runoranj.com>
2019-08-06 19:38:01 -0500

    D | Update.

--- d/web-server/source/app.d
+++ d/web-server/source/app.d
@@ -1,14 +1,20 @@
 import std.stdio;
 import vibe.d;
 
+void mainHandler(HTTPServerRequest req, HTTPServerResponse res) {
+  res.writeBody("Hello: " ~ req.path);
+}
+
 void main() {
   writeln("Starting server.");
 
   auto settings = new HTTPServerSettings();
   settings.port = 8080;
   
-  listenHTTP(settings, (req, res) {
-      res.writeBody("Hello Vibe.d: " ~ req.path);
-  });
+  listenHTTP(settings, &mainHandler);
   runApplication();
 }
+
+// Local Variables:
+// compile-command: "cd .. && dub run"
+// End:
