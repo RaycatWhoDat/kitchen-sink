87f5f7a2c2fd9849b50040c7ecf1b122611dfe9e
Ray Perry <rperry@runoranj.com>
2019-08-27 09:07:10 -0500

    Adhoc | Bottles of beer.

---
+++ d/bottles-of-beer.d
@@ -0,0 +1,34 @@
+#!/usr/bin/env rdmd
+
+module beer;
+
+import std.stdio;
+import std.getopt;
+import std.conv;
+
+// 99 bottles of beer on the wall, 99 bottles of beer.
+// Take one down and pass it around, 98 bottles of beer on the wall.
+
+// 1 bottle of beer on the wall, 1 bottle of beer.
+// Go to the store and buy some more, 99 bottles of beer on the wall.
+ 
+auto pluralize = (int number) => (number > 1) ? "bottles" : "bottle";
+
+string getVerse(int startingNumber, int remainingBottles) {
+  int currentNumber = remainingBottles > 1 ? remainingBottles - 1 : startingNumber;
+  return ((remainingBottles > 1) ? "Take one down and pass it around, " : "Go to the store and buy some more, ") ~
+      currentNumber.to!string() ~ " " ~ pluralize(currentNumber) ~ " of beer on the wall.";
+}
+      
+void main(string[] args) {
+  if (args.length <= 0) return;
+
+  int bottlesOfBeer = 10;
+  getopt(args, "bottles", &bottlesOfBeer);
+
+  for (int remainingBottles = bottlesOfBeer; remainingBottles > 0; remainingBottles--) {
+    writeln(remainingBottles.to!string() ~ " " ~ pluralize(remainingBottles) ~ " of beer on the wall, " ~
+            remainingBottles.to!string()  ~ " " ~ pluralize(remainingBottles) ~ " of beer.");
+    writeln(getVerse(bottlesOfBeer, remainingBottles));
+  }
+}
