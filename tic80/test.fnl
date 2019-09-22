249c4f158eaa00230085c9833739704a1876d80f
Ray Perry <rperry@runoranj.com>
2019-09-21 23:01:19 -0500

    Lua, Fennel, JavaScript | Testing some UI display effects.

---
+++ tic80/test.fnl
@@ -0,0 +1,9 @@
+;; title: test
+;; desc: Another test
+;; script: fennel
+
+(global TIC
+  (lambda []
+    "Main loop."
+    (cls 12)
+    (print "This is a test." 84 64)))
---
+++ tic80/test.js
@@ -0,0 +1,14 @@
+// title: test.js
+// desc: Just a test.
+// script: js
+
+// No fat arrows.
+
+const TIC = function() {
+    cls(12);
+    print('Hello, world.', 84, 64);
+}
+
+// Local Variables:
+// compile-command: "tic -code test.js"
+// End:
