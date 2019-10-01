aadfcb4984055d02e61032387d68215d0badfcb0
Ray Perry <rmperry09@gmail.com>
2019-09-30 20:23:48 -0500

    Haxe | Small update.

--- haxe/src/hello-hashlink/Hello.hx
+++ haxe/src/hello-hashlink/Hello.hx
@@ -1,16 +1,24 @@
 package;
 
+import hxd.*;
+
 class Hello extends hxd.App {
-    override function init() {
-        var message = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
-        message.text = "Hello, HashLink!";
-        message.x = 10;
-        message.y = 10;
-    }
-    
-    static function main() {
-        new Hello();
-    }
+  override function init() {
+    var message = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
+    message.text = "Hello, HashLink!";
+    message.dropShadow = { dx: 0.5, dy: 0.5, color: 0x00FF00, alpha: 0.8 };
+    message.x = 10;
+    message.y = 10;
+    message.scale(7);
+  }
+
+  override function update(dt: Float) {
+    if (Key.isDown(Key.Q)) System.exit();
+  }
+  
+  static function main() {
+    new Hello();
+  }
 }
 
 // Local Variables:
