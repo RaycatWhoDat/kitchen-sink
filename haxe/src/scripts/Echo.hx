1b109c66d1af146ae70cdcde5bd531c552efc1e7
Ray Perry <rperry@runoranj.com>
2019-10-18 09:45:46 -0500

    Haxe | Scripts.

---
+++ haxe/src/scripts/Echo.hx
@@ -0,0 +1,11 @@
+import sys.FileSystem.*;
+
+class Echo {
+    public static function main() {
+        trace("Done again.");
+    }
+}
+
+// Local Variables:
+// compile-command: "haxe --main Echo --interp"
+// End:
