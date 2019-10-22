62e4288c2468cb3c0a9311fd2e2824a53e3db669
Ray Perry <rperry@runoranj.com>
2019-10-22 12:04:22 -0500

    Haxe | We got macros, boys.

--- haxe/src/scripts/MacroTesting.hx
+++ haxe/src/scripts/MacroTesting.hx
@@ -1,14 +1,10 @@
 package;
 
 import TestMacros.*;
+import RangeTools.*;
 
 class MacroTesting {
     public static function main() {
-        inline function range(min: Int, max: Int) {
-            return [for (_ in min...max) _];
-        };
-
-        trace(zip([0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11]));
-        untyped __js__("console.log('we here')");
+        trace(zip(range(0, 5), [4, 5, 6, 7, 8, 9], range(9, 14)));
     }
 }
--- haxe/src/scripts/TestMacros.hx
+++ haxe/src/scripts/TestMacros.hx
@@ -2,28 +2,48 @@ package;
 
 import haxe.macro.Context;
 import haxe.macro.Expr;
+import RangeTools;
 using haxe.macro.Tools;
 
 class TestMacros {
-  public static macro function printSomething(?message: ExprOf<String>) {
-    return macro {
-      trace($message);
-    };
-  }
+    public static macro function printSomething(?message: ExprOf<String>) {
+        return macro {
+            trace($message);
+        };
+    }
+    
+    public static macro function zip(sequences: Array<Expr>) {
+        var numberOfElementsInFirstSequence = null;
+        var allSequences = [
+            for (sequence in sequences) {
+                switch (sequence.expr) {
+                case ECall({ pos: _, expr: EConst(CIdent(range)) }, args):
+                    var argValues = [for (arg in args) arg.getValue()];
+                    if (numberOfElementsInFirstSequence == null && argValues.length > 1) {
+                        numberOfElementsInFirstSequence = 1 + argValues[1] - argValues[0];
+                    }
+                    macro RangeTools.range($v{argValues[0]}, $v{argValues[1]});
+                case EArrayDecl(_):
+                    if (numberOfElementsInFirstSequence == null) {
+                        numberOfElementsInFirstSequence = sequence.getValue().length;
+                    }
+                    $v{sequence};
+                default:
+                    $v{sequence};
+                }
+            }
+        ];
 
-  public static macro function zip(sequences: Array<Expr>) {
-      var allSequences = [for (sequence in sequences) $v{sequence}];
-      var numberOfSequences = allSequences.length;
-      var numberOfElementsInFirstSequence = allSequences[0].getValue().length;
-      
-      return macro {
-          [
-              for (xIndex in 0...$v{numberOfElementsInFirstSequence}) {
-                  for (yIndex in 0...$v{numberOfSequences}) {
-                      $a{allSequences}[yIndex][xIndex];
-                  }
-              }
-          ];
-      };
-  }
+        var numberOfSequences = allSequences.length;
+        
+        return macro {
+            [
+                for (xIndex in 0...$v{numberOfElementsInFirstSequence}) {
+                    for (yIndex in 0...$v{numberOfSequences}) {
+                        $a{allSequences}[yIndex][xIndex];
+                    }
+                }
+            ];
+        };
+    }
 }
