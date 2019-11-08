cd83b23396f28e5d7c07bbc04727b701b0143d5e
Ray Perry <rperry@runoranj.com>
2019-11-08 15:11:05 -0600

    Haxe | Changes.

--- emacs-lisp/simple-haxe-mode.el
+++ emacs-lisp/simple-haxe-mode.el
@@ -4,14 +4,10 @@
 ;; Version: 26.1
 
 (setq haxe-keywords
-  '("break" "case" "cast" "catch" "continue" "default" "do" "dynamic" "else" "extern" "false" "final" "for" "if" "in" "inline" "macro" "new" "null" "return" "switch" "this" "throw" "true" "try" "var" "while"))
-
-(setq haxe-literals
-  '("abstract" "class" "enum" "extends" "function" "implements" "import" "interface" "operator" "overload" "override" "package" "private" "public" "static" "typedef" "untyped" "using"))
+  '("break" "case" "cast" "catch" "continue" "default" "do" "dynamic" "else" "extern" "false" "final" "for" "if" "in" "inline" "macro" "new" "null" "return" "switch" "this" "throw" "true" "try" "var" "while" "abstract" "class" "enum" "extends" "function" "implements" "import" "interface" "operator" "overload" "override" "package" "private" "public" "static" "typedef" "untyped" "using"))
 
 (setq simple-haxe-highlights
-  `((,(regexp-opt haxe-literals 'words) . font-lock-builtin-face)
-     (,(regexp-opt haxe-keywords 'words) . font-lock-keyword-face)))
+  `((,(regexp-opt haxe-keywords 'words) . font-lock-keyword-face)))
 
 (defvar simple-haxe-mode-map nil "Keymap for `simple-haxe-mode'.")
 
@@ -21,12 +17,20 @@
   (insert (make-string indentation-level ? )))
 
 (progn
-  (setq simple-haxe-mode-map (make-sparse-keymap))
-  (define-key simple-haxe-mode-map (kbd "RET") 'newline-and-indent)
-  (define-key simple-haxe-mode-map (kbd "<tab>") 'shm/indent))
+  (setq simple-haxe-mode-map (make-sparse-keymap)))
 
 (define-derived-mode simple-haxe-mode fundamental-mode "simple-haxe"
   "Major mode for editing Haxe source code with colors.
 
 \\{simple-haxe-mode-map}"
-  (setq font-lock-defaults '(simple-haxe-highlights)))
+  (setq font-lock-defaults '(simple-haxe-highlights))
+  (setq indent-tabs-mode t)
+  (setq tab-width 2)
+  (setq-default electric-indent-inhibit t)
+  (setq backward-delete-char-untabify-method 'hungry)
+  (whitespace-mode 1)
+  (setq whitespace-style '(face tabs tab-mark trailing))
+  (custom-set-faces
+    '(whitespace-tab ((t (:foreground "#636363")))))
+  (setq whitespace-display-mappings
+    '((tab-mark 9 [124 9] [92 9]))))
