cf139a2508316658936613f87e7d7bd29c9994bd
Ray Perry <rmperry09@gmail.com>
2019-11-15 16:41:41 -0600

    Kotlin | A few updates.

--- kotlin/hello.kt
+++ kotlin/hello.kt
@@ -1,15 +1,29 @@
 package hello;
 
+val suitMapping = mapOf(
+    0 to "♥",
+    1 to "♠",
+    2 to "♦",
+    3 to "♣"
+);
+
 data class Card(val cardValue: Int, val cardSuit: Int)
 
-fun main() {
-    val suitMapping = mapOf(
-        0 to "♥",
-        1 to "♠",
-        2 to "♦",
-        3 to "♣"
-    );
+fun printCard(card: Card) {
+    val (cardValue, cardSuit) = card;
+    val displayedCardSuit = suitMapping.get(cardSuit)
+    val displayedCardValue = when (cardValue) {
+        1 -> "A"
+        11 -> "J"
+        12 -> "Q"
+        13 -> "K"
+        else -> "$cardValue"
+    }
     
+    println("$displayedCardValue$displayedCardSuit")
+}
+
+fun main() {
     (0..51)
         .map {
             val newValue = (it % 13) + 1
@@ -19,20 +33,7 @@ fun main() {
         }
         .shuffled()
         .take(5)
-        .forEach {
-            val (cardValue, cardSuit) = it;
-            
-            val displayedCardSuit = suitMapping.get(cardSuit)
-            val displayedCardValue = when (cardValue) {
-                1 -> "A"
-                11 -> "J"
-                12 -> "Q"
-                13 -> "K"
-                else -> "$cardValue"
-            }
-            
-            println("$displayedCardValue$displayedCardSuit")
-        }
+        .forEach { printCard(it); }
 }
 
 // Local Variables:
