ee525b24b2eb98d750c1c8837cfd819c4c19bc35
Ray Perry <rmperry09@gmail.com>
2019-11-22 20:34:45 -0600

    Emacs Lisp | get-files update.

--- emacs-lisp/get-files.el
+++ emacs-lisp/get-files.el
@@ -2,13 +2,23 @@
   (let ((ignored-paths '("." ".." ".git" "love" "target" "node_modules" "dist" ".dub"))
          (indentation-width 2))
     (dolist (file (directory-files directory-path))
-      (when (not (member file ignored-paths))
-        (insert (concat (make-string (* indentation-width traversal-level) ?\s) file "\n"))
-        (when (f-directory-p (concat directory-path "/" file))
-          (print-files-recursively (concat directory-path "/" file) (1+ traversal-level)))))))
+      (unless (member file ignored-paths)
+        (let ((is-directory (f-directory-p (concat directory-path "/" file))))
+          (insert
+            (propertize
+              (concat (make-string (* indentation-width traversal-level) ?\s) file "\n")
+              'font-lock-face
+              (if is-directory 'font-lock-keyword-face 'font-lock-builtin-face)))
+          (when is-directory
+            (print-files-recursively
+              (concat directory-path "/" file)
+              (1+ traversal-level))))))))
 
-(progn
+(defun prompt-for-path (current-directory)
+  "Prompt for CURRENT-DIRECTORY and list files."
+  (interactive "sPath to list out: ")
+  (kill-buffer "*File Listing*")
   (switch-to-buffer-other-window "*File Listing*")
-  (erase-buffer)
-  (print-files-recursively ".." 0)
-  (special-mode))
+  (print-files-recursively current-directory 0)
+  (special-mode)
+  (font-lock-mode))
