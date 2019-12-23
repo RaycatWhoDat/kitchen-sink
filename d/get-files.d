17af1643c48e98638a2e0edfb0597925e88ce0df
Ray Perry <rmperry09@gmail.com>
2019-12-22 19:38:30 -0600

    D | One true brace.

--- d/get-files.d
+++ d/get-files.d
@@ -14,18 +14,22 @@ const int TWO_SPACES = 2, FOUR_SPACES = 4;
 
 string[] ignoredPaths = [".git", "node_modules", "target", "love", "dist", ".dub", "build", "_build"];
 
-void printFiles(string directoryPath, int traversalLevel = 0) {
-  foreach (entry; dirEntries(directoryPath, SpanMode.shallow)) {
+void printFiles(string directoryPath, int traversalLevel = 0)
+{
+  foreach (entry; directoryPath.dirEntries(SpanMode.shallow))
+  {
     string currentEntry = entry.name.replace(directoryPath, "").replace("/", "");
     if (ignoredPaths.canFind(currentEntry.replace("/", ""))) continue;
-    repeat(INDENTATION_CHARACTER, TWO_SPACES * traversalLevel)
+    INDENTATION_CHARACTER
+        .repeat(TWO_SPACES * traversalLevel)
         .join("")
         .writeln(currentEntry);
-    if (entry.isDir) printFiles(entry.name, traversalLevel + 1);
+    if (entry.isDir) entry.name.printFiles(traversalLevel + 1);
   }
 }
 
-void main(string[] args) {
+void main(string[] args)
+{
   string directoryPath = getcwd();
   getopt(args, "directory", &directoryPath);
   directoryPath.printFiles();
