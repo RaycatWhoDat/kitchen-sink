9954e9c27beeea65624caca4c90988e3083dfa1c
Ray Perry <ray.perry@sonicdrivein.com>
2020-01-13 11:49:09 -0600

    D | get-files update for Windows.

--- d/get-files.d
+++ d/get-files.d
@@ -6,35 +6,50 @@ import std.algorithm: canFind;
 import std.file: dirEntries, getcwd, SpanMode;
 import std.string: replace, join;
 import std.stdio: writeln;
-import std.getopt: getopt;
 import std.range: repeat;
+import std.path: dirSeparator;
+import std.getopt;
 
 const string INDENTATION_CHARACTER = " ";
 const int TWO_SPACES = 2, FOUR_SPACES = 4;
 
 string[] ignoredPaths = [".git", "node_modules", "target", "love", "dist", ".dub", "build", "_build"];
+string directoryPath;
+bool isRecursive = false;
 
 void printFiles(string directoryPath, int traversalLevel = 0)
 {
   foreach (entry; directoryPath.dirEntries(SpanMode.shallow))
   {
-    string currentEntry = entry.name.replace(directoryPath, "").replace("/", "");
-    if (ignoredPaths.canFind(currentEntry.replace("/", ""))) continue;
+    string currentEntry = entry.name;
+    if (directoryPath != ".")
+    {
+      currentEntry = currentEntry.replace(directoryPath, "").replace(dirSeparator, "");
+    }
+    
+    if (ignoredPaths.canFind(currentEntry)) continue;
+
     INDENTATION_CHARACTER
         .repeat(TWO_SPACES * traversalLevel)
         .join("")
         .writeln(currentEntry);
-    if (entry.isDir) entry.name.printFiles(traversalLevel + 1);
+
+    if (isRecursive && entry.isDir) entry.name.printFiles(traversalLevel + 1);
   }
 }
 
 void main(string[] args)
 {
-  string directoryPath = getcwd();
-  getopt(args, "directory", &directoryPath);
+  directoryPath = getcwd();
+
+  getopt(args,
+         std.getopt.config.passThrough,
+         "directory", &directoryPath,
+         "recursive", &isRecursive);
+
   directoryPath.printFiles();
 }
 
 // Local Variables:
-// compile-command: "./get-files.d --directory .."
+// compile-command: "./get-files.d"
 // End:
