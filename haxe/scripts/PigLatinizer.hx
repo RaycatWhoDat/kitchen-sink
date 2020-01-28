601b250ae1771cde6c070e8927e077de22ada95f
Ray Perry <ray.perry@thoughtworks.com>
2020-01-28 16:44:22 -0600

    Haxe | Inline that jawn.

--- haxe/scripts/PigLatinizer.hx
+++ haxe/scripts/PigLatinizer.hx
@@ -3,16 +3,16 @@ package;
 using Lambda;
 
 class PigLatinizer {
-  public static function pigIt(sentence: String): String {
-    return sentence.split(" ").map(word -> {
-      var baseWord = word.substr(1) + word.substr(0, 1);
-      return baseWord + (~/^[a-z]+$/i.match(word) ? "ay" : "");
-    }).join(" ");
-  }
-
   public static function main() {
+    inline function pigIt(sentence: String): String {
+      return sentence.split(" ").map(word -> {
+        var baseWord = word.substr(1) + word.substr(0, 1);
+        return baseWord + (~/^[a-z]+$/i.match(word) ? "ay" : "");
+      }).join(" ");
+    };
+
     var testCases = ["This is a test", "Pig latin is cool", "Hello world !"];
-    testCases.iter(testCase -> trace(PigLatinizer.pigIt(testCase)));
+    testCases.iter(testCase -> trace(pigIt(testCase)));
   }
 }
 // Local Variables:
