fb286b69e4d6700c5dfe62c66a6df485ee1e130e
Ray Perry <ray.perry@thoughtworks.com>
2020-02-21 10:38:13 -0600

    D | Some updates.

--- d/capitalize.d
+++ d/capitalize.d
@@ -2,18 +2,26 @@ module capitalize;
 
 import std.stdio: writeln;
 import std.array: split, join;
-import std.algorithm: map;
+import std.algorithm: each, map;
 import std.ascii: toUpper;
 import std.conv: text;  
 import std.range: front, back;
 
 string capitalizeFirstLast(string input) {  
-    return input
-        .split(" ")
-        .map!(word => text(word.front.toUpper, word[1..$-1], word.back.toUpper))
-        .join(" ");
+  return input
+      .split(" ")
+      .map!(word => text(word.front.toUpper, word[1..$-1], word.back.toUpper))
+      .join(" ");
 }
 
 void main() {
-    "whatever you want".capitalizeFirstLast.writeln;
+  auto testCases = ["whatever", "whatever you", "whatever you want", "yeah man"];
+  
+  testCases
+      .map!capitalizeFirstLast
+      .each!writeln;
 }
+
+// Local Variables:
+// compile-command: "rdmd capitalize.d"
+// End:
