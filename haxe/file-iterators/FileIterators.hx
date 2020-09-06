7720010a67fe13121808fb86b0bf3e1f6c3e6848
Ray Perry <rmperry09@gmail.com>
2020-09-06 09:26:49 -0500

    Haxe | File iterators. | [Ray]

--- haxe/file-iterators/FileIterators.hx
+++ haxe/file-iterators/FileIterators.hx
@@ -2,13 +2,35 @@ package;
 
 using StringUtils;
 
+@:forward(push)
+abstract Range(Array<Any>) from Array<Any> to Array<Any> {
+  public function new(items: Array<Any>) {
+    this = items;
+  }
+
+  @:arrayAccess function getByIterator(iterator: IntIterator) {
+    var slice = [];
+    for (index in iterator) {
+      try {
+        slice.push(this[index]);
+      } catch (error) {
+        throw new haxe.Exception('Index $index is out-of-bounds.');
+      }
+    }
+    return slice;
+  }
+}
+  
 @:tink class FileIterators {
   public static function main() {
     var mockData = "MOCK_DATA.csv".byLine();
     var mockData2 = "MOCK_DATA.csv".byLine();
     var mockData3 = "MOCK_DATA.csv".byLine();
-    
-    var lines = [for ([line in mockData, line2 in mockData2, line3 in mockData3]) [line, line2, line3]].slice(0,3);
-    trace(lines);
+      
+    var lines: Range = [];
+    for ([line in mockData, line2 in mockData2, line3 in mockData3]) {
+      lines.push([line, line2, line3]);
+    };
+    trace(lines[0...3]);
   }
 }
