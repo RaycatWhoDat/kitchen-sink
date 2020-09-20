03fda8c743e8116907c4473dd76284bf00c86fb7
Ray Perry <ray.perry@thoughtworks.com>
2020-09-20 09:28:58 -0500

    Common Lisp | Updates. | [Ray]

--- common-lisp/capitalize-first-last.lisp
+++ common-lisp/capitalize-first-last.lisp
@@ -1,14 +1,14 @@
 (defun capitalize-first-last (input)
     "Given INPUT is a space-delimited string, capitalize the first and last letter of each word."
-    (loop 
-        for letter across input
-		for index from 0
-		collect (if (or (zerop index) 
-			            (= (1+ index) (length input))
-                        (char= (char input (1- index)) #\Space)
-                        (char= (char input (1+ index)) #\Space))
+  (loop
+    for letter across input
+    for index from 0
+    collect (if (or (zerop index) 
+                  (= (1+ index) (length input))
+                  (char= (char input (1- index)) #\Space)
+                  (char= (char input (1+ index)) #\Space))
                     (string-upcase letter)
-                    (string-downcase letter)) into result
-		finally (return (format nil "~{~A~^~}" result))))
+              (string-downcase letter)) into result
+    finally (return (format nil "~{~A~^~}" result))))
 
 (print (capitalize-first-last "whatever you want"))
