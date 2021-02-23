a6d0223835c82b2dcdcb2ecaa8cfe37a7eeeb1f0
Ray Perry <rmperry09@gmail.com>
2021-02-22 19:23:21 -0600

    Rust | Updates. | [Ray]

--- rust/get-files/src/main.rs
+++ rust/get-files/src/main.rs
@@ -1,13 +1,18 @@
 // (compile "cargo run ..")
 
-use std::io;
 use std::env::args;
 use std::fs::read_dir;
+use std::io::Result;
 use std::path::{Path, PathBuf};
+use std::string::String;
 
 const TWO_SPACES: usize = 2;
 
-fn get_files(directory_path: &PathBuf, traversal_level: usize) -> io::Result<()> {
+fn get_files(
+    directory_path: &PathBuf,
+    callback: &dyn Fn(&String, &str) -> (),
+    traversal_level: usize,
+) -> Result<()> {
     let ignored_paths = ["node_modules", ".git", "target", "dist", "dub", "love"];
 
     let mut entries = read_dir(directory_path)?
@@ -15,22 +20,28 @@ fn get_files(directory_path: &PathBuf, traversal_level: usize) -> io::Result<()>
         .collect::<Vec<_>>();
 
     entries.sort();
-    
+
     let indentation = " ".repeat(traversal_level * TWO_SPACES);
-    
+
     for entry in entries {
-        let formatted_path = entry.iter().last().unwrap();
-        if ignored_paths.contains(&formatted_path.to_str().unwrap()) { continue; }
-        println!("{}{}", indentation, &formatted_path.to_str().unwrap());
+        let last_path_component = entry.iter().last().unwrap();
+        let formatted_path = last_path_component.to_str().unwrap();
+
+        if ignored_paths.contains(&formatted_path) { continue; }
+
+        callback(&indentation, formatted_path);
+
         if Path::new(&entry).is_dir() {
-            get_files(&entry, traversal_level + 1).unwrap();
+            get_files(&entry, callback, traversal_level + 1).ok();
         }
-    };
+    }
 
     Ok(())
 }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
     let directory_path = PathBuf::from(args().last().unwrap_or(".".to_string()));
-    get_files(&directory_path, 0)
+    let print_item = |indentation: &String, item: &str| println!("{}{}", indentation, item);
+
+    get_files(&directory_path, &print_item, 0)
 }
