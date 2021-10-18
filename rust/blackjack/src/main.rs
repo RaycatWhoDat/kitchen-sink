54a3b635851be296d83a26d2c41f31bcf9bf521b
Ray Perry <ray.perry@enotarylog.com>
2021-10-18 09:47:18 -0500

    Rust | Blackjack. | [Ray]

---
+++ rust/blackjack/src/main.rs
@@ -0,0 +1,64 @@
+use std::fmt::{Display, Formatter, Result};
+
+struct Card {
+    display_string: String,
+    value: [usize; 2]
+}
+
+impl Display for Card {
+    fn fmt(&self, formatter: &mut Formatter) -> Result {
+        write!(formatter, "{} {:?}", self.display_string, self.value)?;
+        Ok(())
+    }
+}
+
+impl Card {
+    fn new(card_value: usize, suit_index: usize) -> Card {
+        let suits = vec!["♥", "♠", "♦", "♣"];
+        let royals = vec!["J", "Q", "K"];
+
+        let suit = suits[suit_index];
+        
+        match card_value {
+            1 => Card {
+                display_string: format!("A{}", suit),
+                value: [1, 11]
+            },
+            2..=10 => Card {
+                display_string: format!("{}{}", card_value, suit),
+                value: [card_value; 2]
+            },
+            11 | 12 | 13 => Card {
+                display_string: format!("{}{}", royals[card_value - 11], suit),
+                value: [10; 2]
+            },
+            _ => Card {
+                display_string: String::new(),
+                value: [0; 2]
+            }
+        }
+    }
+}
+
+struct Deck {
+    cards: Vec<Card>
+}
+
+impl Deck {
+    fn new() -> Deck {
+        let mut deck = Deck { cards: vec![] };
+        deck.generate();
+        deck
+    }
+    
+    fn generate(&mut self) {
+        self.cards = (0..=51)
+            .map(|index| Card::new((index % 13) + 1, index / 13))
+            .collect();
+    }
+}
+
+fn main() {
+    let deck = Deck::new();
+    println!("{}", deck.cards[13]);
+}
