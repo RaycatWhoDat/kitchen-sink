697c2b11cc923f27c72c33f2dde1b24ae3b7d6b7
Ray Perry <ray.perry@enotarylog.com>
2022-02-04 22:31:34 -0600

    NOJIRA | Rust stuff. | [Ray]

--- rust/wordle/src/main.rs
+++ rust/wordle/src/main.rs
@@ -1,37 +1,36 @@
 use std::io;
 use std::io::Write;
+use rand::seq::SliceRandom;
 
 #[derive(Clone)]
 struct Guess {
-    word: String,
+    word: Vec<char>,
     letters: [bool; 5],
     positions: [bool; 5]
 }
 
 impl Guess {
     fn is_correct(&self) -> bool {
-        let mut is_correct = true;
-        for position in &self.positions {
-            if !position {
-                is_correct = false;
-                break;
-            }
-        }
-        is_correct
+       self.positions.iter().all(|position| *position)
     }
 }
 
 struct GameState {
-    chosen_word: String,
-    valid_letters: Vec<String>,
+    chosen_word: Vec<char>,
     guesses: Vec<Guess>
 }
 
-fn read_line() -> String {
+fn read_line() -> Option<String> {
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer).unwrap();
     buffer.truncate(5);
-    String::from(buffer.trim())
+    buffer = String::from(buffer.trim());
+    if buffer.len() < 5 {
+        println!("Sorry! You can only guess 5-letter words.");
+        None
+    } else {
+        Some(buffer)
+    }
 }
 
 fn main() {
@@ -39,34 +38,55 @@ fn main() {
     let ansi_green = String::from("\x1B[30;1m\x1B[42m");
     let ansi_yellow = String::from("\x1B[30;1m\x1B[43m");
     let ansi_red = String::from("\x1B[31m\x1B[40m");
+
+    let mut random_words = vec![
+        "shard",
+        "piece",
+        "tower",
+        "those",
+        "moist"
+    ];
+
+    let mut rng = rand::thread_rng();
+    random_words.shuffle(&mut rng);
+    let chosen_word = random_words[0];
     
     let mut state = GameState {
-        chosen_word: String::from("shard"),
-        valid_letters: vec![],
+        chosen_word: chosen_word.chars().collect(),
         guesses: vec![]
     };
 
-    for character in state.chosen_word.chars() {
-        state.valid_letters.push(String::from(character));
-    }
-    
     println!("Werdle!");
     loop {
-        print!("Please enter a word: ");
-        io::stdout().flush().expect("Something went horribly wrong.");
-        let input = read_line();
+        let input: String;
+
+        loop {
+            print!("Please enter a word: ");
+            io::stdout().flush().expect("Something went horribly wrong.");
+            if let Some(raw_input) = read_line() {
+                input = raw_input;
+                break;
+            } 
+        }
 
         let mut guess = Guess {
-            word: input,
+            word: input.chars().collect(),
             letters: [false; 5],
             positions: [false; 5]
         };
 
+        let mut valid_letters = state.chosen_word.clone();
+
+        println!("{:?}", valid_letters);
+        
         // Check word here.
-        for (index, character) in guess.word.chars().enumerate() {
-            let letter = String::from(character);
-            let is_valid_position = state.chosen_word.get(index..=index).unwrap() == letter;
-            let is_valid_letter = is_valid_position || state.valid_letters.contains(&letter);
+        for (index, letter) in guess.word.iter().enumerate() {
+            let is_valid_position = state.chosen_word[index] == *letter;
+            let is_valid_letter = is_valid_position || valid_letters.iter().any(|valid_letter| valid_letter == letter);
+            if is_valid_position || is_valid_letter {
+                valid_letters.retain(|valid_letter| valid_letter != letter);
+            };
+            
             guess.letters[index] = is_valid_letter;
             guess.positions[index] = is_valid_position;
         }
@@ -75,7 +95,7 @@ fn main() {
 
         println!("===============");
         print!("Guess #{}: ", state.guesses.len());
-        for (index, letter) in guess.word.chars().enumerate() {
+        for (index, letter) in guess.word.iter().enumerate() {
             if guess.positions[index] {
                 print!("{}{}{}", ansi_green, letter, ansi_reset);
             } else if guess.letters[index] {
