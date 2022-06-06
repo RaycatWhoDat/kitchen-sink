e87edd49dbb9e57653c797e09979404231e1b6c3
Ray Perry <ray.perry@enotarylog.com>
2022-06-06 08:21:50 -0500

    Ruby | Remove parens? | [Ray]

--- ruby/tapster.rb
+++ ruby/tapster.rb
@@ -1,11 +1,11 @@
-class Card < Struct.new(:number, :cardholder_name, :balance, :ounces_poured)
-  def initialize(number, cardholder_name, balance = 0.00, ounces_poured = 0.00)
+class Card < Struct.new :number, :cardholder_name, :balance, :ounces_poured
+  def initialize number, cardholder_name, balance = 0.00, ounces_poured = 0.00
     super
   end
 end
 
-class ReaderEvent < Struct.new(:type, :payload, :timestamp)
-  def initialize(type, payload, timestamp = Time.now.to_i)
+class ReaderEvent < Struct.new :type, :payload, :timestamp
+  def initialize type, payload, timestamp = Time.now.to_i
     super
   end
 end
@@ -17,21 +17,21 @@ class Reader
     @events = []
   end
 
-  def insert_card(card)
-    @events.push(ReaderEvent.new(:inserted, card.cardholder_name))
+  def insert_card card
+    @events.push ReaderEvent.new :inserted, card.cardholder_name
     @current_card = card
   end
 
-  def charge_card(ounces_poured, price_per_ounce)
+  def charge_card ounces_poured, price_per_ounce
     new_charge = ounces_poured * price_per_ounce
     
-    @events.push(ReaderEvent.new(:charged, new_charge))
+    @events.push ReaderEvent.new :charged, new_charge
     @current_card.ounces_poured = ounces_poured
     @current_card.balance += new_charge
   end
 
   def remove_card
-    @events.push(ReaderEvent.new(:removed, @current_card.cardholder_name))
+    @events.push ReaderEvent.new :removed, @current_card.cardholder_name
     @current_card = nil
   end
 
@@ -43,10 +43,10 @@ class Reader
   end
 end
 
-card = Card.new("5555555555555555", "Ray Perry")
+card = Card.new "5555555555555555", "Ray Perry"
 reader = Reader.new
-reader.insert_card(card)
-reader.charge_card(10.1, 0.79)
+reader.insert_card card
+reader.charge_card 10.1, 0.79
 reader.display_stats
 reader.remove_card
 
