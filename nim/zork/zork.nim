d534c557f254e9ab12735cfed25dd3d5c3dfd02c
Ray Perry <rmperry09@gmail.com>
2022-09-25 21:06:57 -0500

    Nim | WIP, Zork. | [Ray]

---
+++ nim/zork/zork.nim
@@ -0,0 +1,38 @@
+import std/[sequtils, sugar, tables]
+
+type Direction = enum NORTH, EAST, SOUTH, WEST
+
+var directions = @[
+  Direction.NORTH,
+  Direction.EAST,
+  Direction.SOUTH,
+  Direction.WEST
+]
+
+type Room = ref object
+  numberOfRooms: int
+  name: string
+  description: string
+  rooms: Table[Direction, Room]
+
+proc getExits(room: Room): seq[Direction] =
+  collect:
+    for direction in directions:
+      if direction in room.rooms:
+        direction
+
+when isMainModule:
+  var room1 = Room(
+    name: "The Front Room",
+    description: "The Front Room",
+    rooms: initTable[Direction, Room]()
+  )
+  var room2 = Room(
+    name: "The Back Room",
+    description: "The Back Room",
+    rooms: { SOUTH: room1 }.toTable
+  )
+  echo room1.getExits
+  room1.rooms[NORTH] = room2
+  echo room1.getExits
+  
