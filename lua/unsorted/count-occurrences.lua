d2fc6f0bc14b045330d10d5eaf14577bef6165f8
Ray Perry <rmperry09@gmail.com>
2022-12-28 22:02:52 -0600

    Lua | Blackjack. | [Ray]

---
+++ lua/unsorted/count-occurrences.lua
@@ -0,0 +1,6 @@
+occurrences = 0
+for line in string.gmatch(io.input("../../awk/test-text.txt"):read("*a"), "TXR") do
+   occurrences = occurrences + 1
+end
+
+print("Number of TXR occurrences: " .. occurrences)
