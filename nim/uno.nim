db4b9d292a71188e645ee1f01d02fcd53dfddbf6
Ray Perry <rmperry09@gmail.com>
2023-03-24 23:33:54 -0500

    Nim | Uno. | [Ray]

--- nim/uno.nim
+++ nim/uno.nim
@@ -17,7 +17,7 @@ type Effect = enum
   DrawTwo
   Wild
   WildDrawFour
-  
+
 type Card = ref object
   color: Option[Color]
   number: Option[int]
@@ -32,28 +32,32 @@ type Deck = ref object of Hand
 
 type DiscardPile = ref object
   topCard: Card
-  
+
 proc newDeck(): Deck =
   result = Deck(cards: @[], counter: 0)
   # Generate non-Wild cards
   for color in Color:
     for number in 0..9:
-      let newCard = Card(color: some(color), number: some(number), effect: none(Effect), isPlayable: false)
+      let newCard = Card(color: some(color), number: some(number), effect: none(
+          Effect), isPlayable: false)
       let numberOfCopies = if number > 0: 2 else: 1
       result.cards = result.cards & repeat(newCard, numberOfCopies)
     for effect in Effect:
-      let newCard = Card(color: some(color), number: none(int), effect: some(effect), isPlayable: false)
+      let newCard = Card(color: some(color), number: none(int), effect: some(
+          effect), isPlayable: false)
       case effect:
         of Skip, DrawTwo, Reverse:
           result.cards = result.cards & repeat(newCard, 2)
         else:
           break
   # Add the Wilds
-  let wildCard = Card(color: none(Color), number: none(int), effect: some(Wild), isPlayable: false)
-  let wildDrawFourCard = Card(color: none(Color), number: none(int), effect: some(WildDrawFour), isPlayable: false)
+  let wildCard = Card(color: none(Color), number: none(int), effect: some(Wild),
+      isPlayable: false)
+  let wildDrawFourCard = Card(color: none(Color), number: none(int),
+      effect: some(WildDrawFour), isPlayable: false)
   result.cards = result.cards & cycle(@[wildCard, wildDrawFourCard], 4)
   shuffle(result.cards)
-  
+
 proc drawCard(deck: Deck): Card =
   result = deck.cards[deck.counter]
   deck.counter = if deck.counter >= deck.cards.len(): 0 else: deck.counter + 1
@@ -63,23 +67,21 @@ proc discardToPile(card: Card, discardPile: DiscardPile) =
     # Assign color
     card.color = some(Red)
   discardPile.topCard = card
-  
+
 proc newHand(deck: Deck): Hand =
   result = Hand(cards: @[])
   for index in 0..<7:
     result.cards.add(deck.drawCard())
-  
+
 proc printCard(card: Card) =
-  var fgColor = fgBlack
+  var fgColor = fgWhite
   var bgColor = bgBlack
   var value: string
   case card.color:
     of Some(@color):
       bgColor = parseEnum[BackgroundColor]("bg" & $color)
-    of None():
-      bgColor = bgBlack
-      fgColor = fgWhite
-
+      fgColor = fgBlack
+    
   case card.effect:
     of Some(Skip):
       value = "SK"
@@ -95,14 +97,14 @@ proc printCard(card: Card) =
   case card.number:
     of Some(@number):
       value = " " & $number
-    of None():
-      break
 
   stdout.styledWrite(styleBright, bgColor, fgColor, " ", value, " ", resetStyle)
 
 proc checkIfPlayable(card: Card, discardPile: DiscardPile) =
-  card.isPlayable = card.color.isNone or (card.effect.isSome and card.effect == discardPile.topCard.effect) or (card.number.isSome and card.number == discardPile.topCard.number) or card.color == discardPile.topCard.color
-  
+  card.isPlayable = card.color.isNone or (card.effect.isSome and card.effect ==
+      discardPile.topCard.effect) or (card.number.isSome and card.number ==
+      discardPile.topCard.number) or card.color == discardPile.topCard.color
+
 proc status(hand: Hand, discardPile: DiscardPile) =
   stdout.writeLine("=== " & $hand.cards.len() & " cards left ===")
   for card in hand.cards:
@@ -117,7 +119,7 @@ proc status(discardPile: DiscardPile) =
   stdout.writeLine("=== Top card is: ===")
   discardPile.topCard.printCard()
   echo ""
-    
+
 when isMainModule:
   let deck = newDeck()
   let hand = newHand(deck)
