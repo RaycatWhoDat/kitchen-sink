352bfa9d796789ec94aa5b429ce8b7b338a8cf2c
Ray Perry <rmperry09@gmail.com>
2023-03-30 13:36:05 -0500

    Nim | Parse text. | [Ray]

--- nim/parsetext.nim
+++ nim/parsetext.nim
@@ -1,12 +1,16 @@
-import std/parseutils
+from std/strutils import Digits
+from std/parseutils import parseInt, skipUntil
+import std/enumerate
 
 proc main =
   var file = open("../README.md", fmRead)
   defer: close(file)
-  for line in file.lines:
+  for index, line in enumerate(file.lines):
     var value: int
-    discard parseInt(line, value)
-    echo line
-    echo value
+    let skippedCharacters = skipUntil(line, Digits)
+    if skippedCharacters != 0 and skippedCharacters != line.len:
+      discard line.parseInt(value, skippedCharacters)
+      echo "L" & $index & ": " & $value
+      echo "Context: " & line & "\n"
 
 main()
