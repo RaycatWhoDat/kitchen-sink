1c6961daff2dd5fc14287c333a6ed76ba7bf5a19
Ray Perry <rmperry09@gmail.com>
2023-04-07 13:34:40 -0500

    C++ | Updates. | [Ray]

--- cpp/snippets/inventory.cpp
+++ cpp/snippets/inventory.cpp
@@ -2,35 +2,75 @@
 #include <string>
 #include <map>
 
-#include "inventory.hpp"
+class InventoryItem
+{
+public:
+  std::string id;
+  std::string name;
+  unsigned int price;
+  InventoryItem(std::string _id, std::string _name, unsigned int _price) : id(_id), name(_name), price(_price) {}
+};
 
-void Store::updateItemQuantity(InventoryItem item, unsigned int amount) {
-  stock[item.id] = amount;
-}
+enum StoreEventType
+{
+  PURCHASED,
+  REFUNDED
+};
 
-void Store::purchaseItem(InventoryItem item) {
-  if (stock[item.id] <= 0) {
-    return;
+class StoreEvent
+{
+public:
+  StoreEventType eventType;
+  unsigned int timestamp;
+  std::string payload;
+  StoreEvent(StoreEventType _eventType, std::string _payload) : eventType(_eventType), payload(_payload) {
+    timestamp = 0;
   }
+};
 
-  StoreEvent storeEvent = StoreEvent(PURCHASED, item.id);
-  events.push_back(storeEvent);
+class Store
+{
+  unsigned int openingTime;
+  unsigned int closingTime;
+  std::map<std::string, unsigned int> stock;
+  std::vector<StoreEvent> events;
 
-  updateItemQuantity(item, stock[item.id] - 1);
-}  
+public:
+  std::string name;
 
-void Store::refundItem(InventoryItem item) {
-  StoreEvent storeEvent = StoreEvent(REFUNDED, item.id);
-  events.push_back(storeEvent);
-
-  updateItemQuantity(item, stock[item.id] + 1);
-}
-
-void Store::getStoreStatus() {
-  for (const auto& entry : stock) {
-    std::cout << entry.first << ": " << entry.second << std::endl;
+  Store(std::string _name) : name(_name) {
+    openingTime = 900;
+    closingTime = 1700;
   }
-}
+  
+  void updateItemQuantity(InventoryItem* item, unsigned int amount) {
+    stock[item->id] = amount;
+  }
+  
+  void purchaseItem(InventoryItem* item) {
+    if (stock[item->id] <= 0) {
+      return;
+    }
+
+    StoreEvent storeEvent = StoreEvent(PURCHASED, item->id);
+    events.push_back(storeEvent);
+
+    updateItemQuantity(item, stock[item->id] - 1);
+  }
+  
+  void refundItem(InventoryItem* item) {
+    StoreEvent storeEvent = StoreEvent(REFUNDED, item->id);
+    events.push_back(storeEvent);
+
+    updateItemQuantity(item, stock[item->id] + 1);
+  }
+  
+  void getStoreStatus() {
+    for (const auto& entry : stock) {
+      std::cout << entry.first << ": " << entry.second << std::endl;
+    }
+  }
+};
 
 int main() {
   InventoryItem item1 = InventoryItem("1", "Item 1 - A", 500);
@@ -39,11 +79,15 @@ int main() {
 
   Store store = Store("Bob's Shop");
   
-  store.updateItemQuantity(item1, 10);
-  store.updateItemQuantity(item2, 7);
-  store.updateItemQuantity(item3, 5);
+  store.updateItemQuantity(&item1, 10);
+  store.updateItemQuantity(&item2, 7);
+  store.updateItemQuantity(&item3, 5);
 
-  store.purchaseItem(item1);
+  store.purchaseItem(&item1);
 
   store.getStoreStatus();
 }
+
+// Local Variables:
+// compile-command: "g++ -std=c++11 -Wall -Wextra -fsanitize=address -o inventory inventory.cpp && ./inventory"
+// End:
